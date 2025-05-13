32e32541a750ffd798abaa7e2872400b3d347196
KenzaDigital <kenza.chabane4@gmail.com>
2025-05-13 11:10:28 +0200

    Decoration

--- Assets/_Glitchy Cat Let Me Work!/_Project/Scripts/UI/ProductivityMeter.cs
+++ Assets/_Glitchy Cat Let Me Work!/_Project/Scripts/UI/ProductivityMeter.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MailManager : MonoBehaviour
+public class ProductivityMeter : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
