9d90a0242281a0bba1af652e640c48950d0f9a83
KenzaDigital <kenza.chabane4@gmail.com>
2025-05-22 10:19:36 +0200

    CuttingBoardReactive

--- Assets/_Glitchy Cat Let Me Work!/_Project/Scripts/MiniGame/PauseDej/CuttingBoard.cs
+++ Assets/_Glitchy Cat Let Me Work!/_Project/Scripts/MiniGame/PauseDej/CuttingBoard.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class CuttingBoard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private SpriteRenderer sr;
+    private Color originalColor;
+    public Color flashColor = Color.yellow; // Couleur flash
+    public float flashDuration = 0.2f;
+
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Ingredient"))
+        {
+            var drag = other.GetComponent<DraggableObject>();
+            if (drag != null) drag.SetOverBoard(true);
+
+            FlashEffect(); // effet visuel rapide
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ingredient"))
+        {
+            var drag = other.GetComponent<DraggableObject>();
+            if (drag != null) drag.SetOverBoard(false);
+        }
+    }
+
+    void FlashEffect()
+    {
+        CancelInvoke();
+        sr.color = flashColor;
+        Invoke(nameof(ResetColor), flashDuration);
+    }
+
+    void ResetColor()
+    {
+        sr.color = originalColor;
     }
 }
